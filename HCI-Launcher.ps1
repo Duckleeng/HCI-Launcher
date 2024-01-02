73504f0a8c46827b33b0256ee59427050b2defc4
Duckling <105670300+Duckleeng@users.noreply.github.com>
2024-01-02 15:56:49 +0100

    Upload script

---
+++ HCI-Launcher.ps1
@@ -0,0 +1,64 @@
+########################################################### CONFIG ###########################################################
+
+$memtest = "memtest.exe"
+$threads = $null
+$affinity = $null
+
+########################################################### CONFIG ###########################################################
+
+if($null -eq $threads)
+{
+    Write-Host "If you wish to skip this prompt, you can adjust the number of threads in the config section of the .ps1 file"
+    Write-Host "Threads: " -NoNewline
+    $threads = Read-Host
+}
+
+try
+{
+    $threads = [int]$threads
+    if($threads -lt 1)
+    {
+        throw
+    }
+}
+catch
+{
+    Write-Error -Message "[ERROR] Number of threads must be a positive integer" -ErrorAction Stop
+}
+
+$cores = (Get-WmiObject Win32_Processor).NumberOfLogicalProcessors
+
+if($null -eq $affinity)
+{
+    Write-Host "Running $threads threads across ALL cores..."
+    for($i = 0; $i -lt $threads; $i++)
+    {
+        $aff = $i % $cores
+        $p = Start-Process -FilePath $memtest -PassThru
+        $p.ProcessorAffinity = [int][Math]::Pow(2, $aff)
+    }
+}else
+{
+    try
+    {
+        for($i = 0; $i -lt $affinity.Count; $i++)
+        {
+            $temp = [int]$affinity[$i]
+            if(($temp -lt 0) -or ($temp -gt $cores-1))
+            {
+                throw
+            }
+        }
+    }catch
+    {
+        Write-Error -Message "[ERROR] Invalid affinity requested" -ErrorAction Stop
+    }
+
+    Write-Host "Running $threads threads across the following cores: $affinity"
+    for($i = 0; $i -lt $threads; $i++)
+    {
+        $aff = $affinity[$i % $affinity.Count]
+        $p = Start-Process -FilePath $memtest -PassThru
+        $p.ProcessorAffinity = [int][Math]::Pow(2, $aff)
+    }
+}
